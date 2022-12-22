f88e2c7b74718e78e8c64cac1223968c6c17942a
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-12-22 08:20:54 +0530

    implemented the usecase 4 to retrieve all data in employee payroll table

--- SQLQuery_Day32.sql
+++ SQLQuery_Day32.sql
@@ -5,3 +5,5 @@ Insert into employee_payroll values('Krishna',50000,'1995-7-12');
 Insert into employee_payroll values('Harish',27000,'1993-4-10');
 Insert into employee_payroll values('Shreya',35000,'1995-5-10');
 Insert into employee_payroll values('Naina',30000,'1995-5-10');
+--------usecase 4--------------
+select * from employee_payroll;
